3f9712001180a432dddcbb8186e81a5031aa5325
Grant Wu <grantwu2@gmail.com>
2015-11-15 13:27:18 -0800

    [SERVER]Get cherrypy and stdlib logging to play nice

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import logging
+import logging.config
 
 import cherrypy
 
-LOGGER = logging.getLogger('CLOUDBIT_SFMUNI')
+from log_conf import LOG_CONF
+
+logging.config.dictConfig(LOG_CONF)
+LOGGER = logging.getLogger(__name__)
 
 class SfMuni(object):
     exposed = True
 
     def GET(self):
+        LOGGER.info('LOGGER INFO')
+        cherrypy.log('CHERRYPY LOG')
         return 'HELLO WORLD'
 
     def POST(self, **kwargs):
@@ -19,7 +25,13 @@ class SfMuni(object):
 
 if __name__ == '__main__':
     cherrypy.config.update({'server.socket_host': '0.0.0.0',
-                            'server.socket_port': 5000})
+                            'server.socket_port': 5000,
+                            'log.screen': False,
+                            'log.access_file': '',
+                            'log.error_file': '',
+                            })
+    cherrypy.engine.unsubscribe('graceful', cherrypy.log.reopen_files)
+
     cherrypy.tree.mount(
         SfMuni(), '/api/hello',
         {'/':
