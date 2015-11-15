91567341be0ec4aae9d8b6345104c51a8ca0932d
Grant Wu <grantwu2@gmail.com>
2015-11-15 15:43:44 -0800

    Add muni stop map file

---
+++ services/muni/stop_map.py
@@ -0,0 +1,9 @@
+"""Dictionary mapping of stops / directions to NextBus API stop tags."""
+
+MAP = {
+    '5': {  # 5-Fulton
+        'market_1st': {
+            'inbound': '5638',
+        },
+    },
+}
