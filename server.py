3d247b7965dd8c69f13f35e763ae0cccc0202195
Grant Wu <grantwu2@gmail.com>
2015-11-15 10:45:25 -0800

    Specify '0.0.0.0' as host
    ...in an attempt to fix the 502 bad gateway errors

--- server.py
+++ server.py
@@ -18,7 +18,8 @@ class SfMuni(object):
 
 
 if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_port': 5000})
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                            'server.socket_port': 5000})
     cherrypy.tree.mount(
         SfMuni(), '/api/hello',
         {'/':
