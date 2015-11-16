60209672080a10423c163be2bc4a59dbff343331
Grant Wu <grantwu2@gmail.com>
2015-11-15 21:21:06 -0800

    [API_MUNI]Spawn new thread for servo output calls

--- routes/muni.py
+++ routes/muni.py
@@ -2,6 +2,7 @@
 
 import logging
 import time
+import threading
 
 import cherrypy
 
@@ -9,6 +10,10 @@ from services.muni import muni
 from services.cloudbit import cloudbit
 
 LOGGER = logging.getLogger()
+ROUTE = '5'
+STOP_NAME = 'market_1st'
+DIRECTION = 'inbound'
+NUM_BUSES = 3
 
 
 class SfMuni(object):
@@ -26,10 +31,10 @@ class SfMuni(object):
 
         # Get muni prediction
         try:
-            next_three = muni.get_next(route='5',
-                                       stop_name='market_1st',
-                                       direction='inbound',
-                                       num=3)
+            next_buses = muni.get_next(route=ROUTE,
+                                       stop_name=STOP_NAME,
+                                       direction=DIRECTION,
+                                       num=NUM_BUSES)
         except muni.NoPredictionsError:
             LOGGER.info('Sweeping face.')
             cloudbit.Motions.sweep_face(bit_id)
@@ -45,16 +50,38 @@ class SfMuni(object):
             cloudbit.Motions.harlem_shake(bit_id)
             return 'NextBus API call failed.'
 
-        for bus_wait in next_three:
-            if bus_wait <= 1:
-                LOGGER.info('Ur not gonna make the next one.')
+        thread = threading.Thread(target=async_cloudbit_output,
+                                  args=(bit_id, next_buses, 3))
 
-            else:
-                # Calculate voltage with response
-                volts_prcnt = cloudbit.get_percentage(bus_wait)
+        LOGGER.info('Spawning new thread.')
+        thread.start()
 
-                # Hit cloudbit
-                cloudbit.output_to_servo(bit_id, volts_prcnt, 3000)
-                time.sleep(3)
+        LOGGER.info('Responding to POST.')
+        return '{route} at {stop_name} {direction} in {next_buses}' \
+            .format(route=ROUTE,
+                    stop_name=STOP_NAME,
+                    direction=DIRECTION,
+                    next_buses=next_buses)
 
-        return str(next_three)
+
+def async_cloudbit_output(bit_id, next_buses, delay):
+    """Iterate over bus times and hit CloudBit with the right output voltages.
+
+    Adds a delay between each output call, to avoid stuttering.
+
+    Arguments:
+        bit_id (str) Unique identifier of the target CloudBit
+        next_buses ([int]) List of bus wait times
+        delay (int) time to wait between CloudBit output calls (seconds)
+    """
+    for bus_wait in next_buses:
+        if bus_wait <= 1:
+            LOGGER.info('Ur not gonna make the next one.')
+
+        else:
+            # Calculate voltage with response
+            volts_prcnt = cloudbit.get_percentage(bus_wait)
+
+            # Hit cloudbit
+            cloudbit.output_to_servo(bit_id, volts_prcnt, delay * 1000)
+            time.sleep(delay)
